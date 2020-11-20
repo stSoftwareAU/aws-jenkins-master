e48e55db56a9361c9dfa12e67c910ef8dddb55fc
Parminder Singh <preetzdomain@gmail.com>
2020-11-20 13:33:13 +1100

    fixes for cloud init logs and Jenkins user id

--- run.sh
+++ run.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
-set -e
-
-sed -i -e 's/#root:.*/root: support@stsoftware.com.au/g' /etc/aliases
-
-# wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
-# rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-
+set -ex
 
 yum update –y
-amazon-linux-extras enable corretto8
-
-yum install -y java-1.8.0-amazon-corretto awslogs amazon-efs-utils jenkins ntp git jq
-
-mkdir -p /home/jenkins
-
-mount -t efs fs-cb20bcf2:/ /home/jenkins
-
-chown -R -v jenkins:jenkins /home/jenkins
-sed --in-place -E "s/( *JENKINS_HOME *=)(.*)/\1\/home\/jenkins/" /etc/sysconfig/jenkins
-
-usermod -s /bin/bash -d /home/jenkins jenkins
-
-ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
-chkconfig ntpd on
+yum install -y awslogs
 
 # Set up logs
 sed --in-place -E "s/( *region *=)(.*)/\1 ap-southeast-2/" /etc/awslogs/awscli.conf
@@ -44,4 +22,31 @@ ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
 aws ec2 associate-address --instance-id $ID --allocation-id eipalloc-0c52226d880174da9 --region ap-southeast-2
 
 systemctl restart awslogsd.service
+
+
+sed -i -e 's/#root:.*/root: support@stsoftware.com.au/g' /etc/aliases
+
+# wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
+# rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+
+amazon-linux-extras enable corretto8
+yum install -y java-1.8.0-amazon-corretto amazon-efs-utils jenkins ntp git jq
+
+set +e
+adduser -u 777 jenkins
+set -e
+mkdir -p /home/jenkins
+usermod --home /home/jenkins jenkins
+mount -t efs fs-cb20bcf2:/ /home/jenkins
+chown -R -v jenkins:jenkins /home/jenkins
+sed --in-place -E "s/( *JENKINS_HOME *=)(.*)/\1\/home\/jenkins/" /etc/sysconfig/jenkins
+
+usermod -s /bin/bash -d /home/jenkins jenkins
+
+ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
+chkconfig ntpd on
+
 service jenkins start
